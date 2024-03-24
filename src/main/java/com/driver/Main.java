68b19ab9dcf97b019bec47c09f9665819899d5c1
Pragya Kumari <pragyakumari121@gmail.com>
2024-03-24 20:01:43 +0530

    changed main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,17 +3,17 @@ package com.driver;
 public class Main {
  static class Product{
      public int product(int x, int y){
-         System.out.println("function with two int parameters and int return type ");
-         return 0;
+         return (x*y);
+
      }
      public int product(int x, int y, int z){
-         System.out.println("function with three int parameters and int return type ");
-         return 0;
+
+         return (x*y*z);
 
      }
      public double product(double x, double y){
-         System.out.println("function with two double parameters and double return type ");
-         return 0.0;
+
+         return (x*y);
 
      }
 
@@ -21,8 +21,9 @@ public class Main {
 
   public static void main(String[] args){
      Product p = new Product();
-     p.product(1,2);
-     p.product(1.2,2.4);
-     p.product(1,2,3);
+    System.out.println( p.product(1,2));
+      System.out.println (p.product(1,2,3));
+     System.out.println(p.product(1.2,2.4));
+
   }
 }
