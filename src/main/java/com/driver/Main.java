9f91da14fe11ebeb8ad5231a1f5b4e5c8cb4323b
Pragya Kumari <pragyakumari121@gmail.com>
2024-03-24 19:49:21 +0530

    1st commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
+ static class Product{
+     public int product(int x, int y){
+         System.out.println("function with two int parameters and int return type ");
+         return 0;
+     }
+     public int product(int x, int y, int z){
+         System.out.println("function with three int parameters and int return type ");
+         return 0;
 
+     }
+     public double product(double x, double y){
+         System.out.println("function with two double parameters and double return type ");
+         return 0.0;
+
+     }
+
+ }
+
+  public static void main(String[] args){
+     Product p = new Product();
+     p.product(1,2);
+     p.product(1.2,2.4);
+     p.product(1,2,3);
+  }
 }
